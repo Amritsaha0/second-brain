99edf66aa4addb16da67b7ec1e72eb4318dd4005
Amritsaha <saha.amrit024@gmail.com>
2025-08-27 19:19:32 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+
+
+const app = express();
+
+app.post('/api/v1/signup', async (req,res)=>{
+
+})
+
+app.post("/api/v1/signin", async (req,res)=>{
+
+
+})
+ 
+app.post("/api/v1/content",(req,res)=>{
+
+})
+
+app.get("/api/v1/content",(req,res)=>{
+    
+})
+
+app.delete("/api/v1/content/:id",(req,res)=>{
+    
+})
+
+app.post("/api/v1/share",(req,res)=>{
+
+})
+
+app.get("/api/v1/share/:id",(req,res)=>{
+
+})
+
+
